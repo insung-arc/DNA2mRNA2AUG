93e859e111f7dff46d06eb1207c3abdfa3b74630
insung3511 <insung.park123@gmail.com>
2019-09-11 22:21:24 +0900

    코드 업로오오드
     몸살이 너무 아파요

---
+++ transcription.py
@@ -0,0 +1,25 @@
+import os
+
+def transcripition(dna):
+    result = dna
+    for x in range(0, len(dna)):
+        if (dna[x] == 'T'):
+            result[x] = 'A'
+
+        if (dna[x] == 'A'):
+            result[x] = 'U'
+            
+        if (dna[x] == 'C'):
+            result[x] = 'G'
+
+        if (dna[x] == 'G'):
+            result[x] = 'C'
+            
+    for x in range(0, len(dna)):
+        print(result[x])
+    
+if __name__ == "__main__":
+    #TACCGGGACA CCTACGCGGA GGACGGGGAC GACCGCGACG ACCGGGAGAC CCCTGGACTG GGTCGGCGTC
+    dna1, dna2, dna3, dna4, dna5, dna6, dna7 = input("Input DNA > ").split()
+    print("[INFO] Start Transcripition...")
+    transcripition(dna1)
