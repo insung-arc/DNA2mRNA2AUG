df18a167bfac0342333cd9c18edf7756de2fc72e
insung3511 <insung.park123@gmail.com>
2019-09-14 09:52:20 +0900

    끝났다..

--- transcription.py
+++ transcription.py
@@ -1,37 +1,49 @@
-import os
 
-whatisthis = {'A' : 'U', 'T' : 'A', 'G' : 'C', 'C' : 'G'}
+file = open('DNA.txt', 'r')
+dna = file.read()
+print("DNA :\n")
+print (dna, "\n")
 
-def transcripition(dna):
-    result = dna
-    for x in range(0, len(dna)):
-        if (result == 'T'):
-            print(whatisthis[result], end=' ')
+rna = ""
+protein_string = ""
 
-        if (dna[:x] == 'A'):
-            print(whatisthis[result], end=' ')
-            
-        if (dna[:x] == 'C'):
-            print(whatisthis[result], end=' ')
+for i in dna:
+    if i == "T":
+        rna += "A"
 
-        if (dna[:x] == 'G'):
-            print(whatisthis[result], end=' ')
+    if i == "A":
+        rna += "U"
 
-            
-    for x in range(0, len(dna)):
-        print(dna[x], end=' ')
+    if i == "C":
+        rna += "G"
 
-    for x in range(0, len(dna)):
-        print(result[x], end=' ')
+    if i == "G":
+        rna += "C"
+
+#print("[INFO] Start Transcripition...\n")
+print("RNA :\n")
+print(rna, "\n")
+
+rna_codon = {
+    "UUU" : "페닐알라닌", "UUC" : "페닐알라닌", "UUA" : "류신", "UUG" :"류신",
+    "CUU" : "류신", "CUU" : "류신", "CUC" : "류신", "CUA" : "류신", "CUG" : "류신",
+    "AUU" : "아이소류신", "AUC" : "아이소류신", "AUA" : "아이소류신", "AUG" : "메싸이오닌(개시코돈)",
+    "GUU" : "발린", "GUC" : "발린", "GUA" : "발린", "GUG" : "발린",
+    "UCU" : "세린", "UCC" : "세린", "UCA" : "세린", "UCG" : "세린",
+    "CCU" : "프롤린", "CCC" : "프롤린", "CCA" : "프롤린", "CCG" : "프롤린",
+    "ACU" : "트레오닌", "ACC" : "트레오닌", "ACA" : "트레오닌", "ACG" : "트레오닌",
+    "GCU" : "알라닌", "GCC" : "알라닌", "GCA" : "알라닌", "GCG" : "알라닌",
+    "UAU" : "타이로신", "UAC" : "타이로신", "UAA" : "[종결코돈]", "UAG" : "[종결코돈]",
+    "CAU" : "히스티딘", "CAC" : "히스티딘", "CAA" : "글루타민", "CAG" : "글루타민",
+    "AAU" : "아스파라진", "AAC" : "아스파라진", "AAA" : "라이신", "AAG" : "라이신",
+    "GAU" : "아스파트산", "GAC" : "아스파트산", "GAA" : "글루탐산", "GAG" : "글루탐산",
+    "UGU" : "시스테인", "UGC" : "시스테인", "UGA" : "[종결코돈]", "UGG" : "트립토판", 
+    "CGU" : "아르지닌", "CGC" : "아르지닌", "CGA" : "아르지닌", "CGG" : "아르지닌",
+    "AGU" : "세린", "AGC" : "세린", "AGA" : "아르지닌", "AGG" : "아르지닌",
+    "GGU" : "글라이산", "GGC" : "글라이신", "GGA" : "글라이신", "GGG" : "글라이신"
+    }
+
+for i in range(0, len(rna) - (3+len(rna)%3), 3):
+    print(rna[i:i+3], " >> ",rna_codon[rna[i:i+3]],"\t")
+
     
-if __name__ == "__main__":
-    #TACCGGGACA CCTACGCGGA GGACGGGGAC GACCGCGACG ACCGGGAGAC CCCTGGACTG GGTCGGCGTC
-    dna1, dna2, dna3, dna4, dna5, dna6, dna7 = input("Input DNA > ").split()
-    print("[INFO] Start Transcripition...")
-    transcripition(dna1)
-    transcripition(dna2)
-    transcripition(dna3)
-    transcripition(dna4)
-    transcripition(dna5)
-    transcripition(dna6)
-    transcripition(dna7)
